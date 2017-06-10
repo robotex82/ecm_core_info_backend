45dbdd0835d7987e68f38cc1c324ae6430a0747a
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-06-11 00:23:27 +0200

    Move to .ruby-* files. Use correct to_prepare hook. Cleanup.

--- config/initializers/extensions.rb
+++ config/initializers/extensions.rb
@@ -2,7 +2,7 @@ if Rails.application.config.cache_classes
   require_dependency Ecm::CoreInfo::Backend::Engine.root.join *%w(extensions bundler remote_specification_extension)
   require_dependency Ecm::CoreInfo::Backend::Engine.root.join *%w(extensions rails engine_extension)
 else
-  ActionDispatch::Callbacks.to_prepare do
+  Rails.application.config.to_prepare do
     require_dependency Ecm::CoreInfo::Backend::Engine.root.join *%w(extensions bundler remote_specification_extension)
     require_dependency Ecm::CoreInfo::Backend::Engine.root.join *%w(extensions rails engine_extension)
   end
