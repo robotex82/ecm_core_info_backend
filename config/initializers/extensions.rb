5c595aaf4ffe67b50561a7d1be270bc5a4aef75b
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-07-15 14:49:26 +0200

    Rails 5.1 compatibility.

--- config/initializers/extensions.rb
+++ config/initializers/extensions.rb
@@ -2,8 +2,15 @@ if Rails.application.config.cache_classes
   require_dependency Ecm::CoreInfo::Backend::Engine.root.join *%w(extensions bundler remote_specification_extension)
   require_dependency Ecm::CoreInfo::Backend::Engine.root.join *%w(extensions rails engine_extension)
 else
-  Rails.application.config.to_prepare do
-    require_dependency Ecm::CoreInfo::Backend::Engine.root.join *%w(extensions bundler remote_specification_extension)
-    require_dependency Ecm::CoreInfo::Backend::Engine.root.join *%w(extensions rails engine_extension)
+  if Rails.version < '5.1'
+    Rails.application.config.to_prepare do
+      require_dependency Ecm::CoreInfo::Backend::Engine.root.join *%w(extensions bundler remote_specification_extension)
+      require_dependency Ecm::CoreInfo::Backend::Engine.root.join *%w(extensions rails engine_extension)
+    end
+  else
+    ActiveSupport::Reloader.to_prepare do
+      require_dependency Ecm::CoreInfo::Backend::Engine.root.join *%w(extensions bundler remote_specification_extension)
+      require_dependency Ecm::CoreInfo::Backend::Engine.root.join *%w(extensions rails engine_extension)
+    end
   end
 end
